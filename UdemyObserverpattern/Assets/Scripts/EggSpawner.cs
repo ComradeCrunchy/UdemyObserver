3f1ee0bfa56df23fb3b6c9441e376821cd140160
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2020-12-10 15:55:35 -0500

    Observer pt4
    Having problem with the icons showing up.

--- UdemyObserverpattern/Assets/Scripts/EggSpawner.cs
+++ UdemyObserverpattern/Assets/Scripts/EggSpawner.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class EggSpawner : MonoBehaviour
 {
     public GameObject eggPrefab;
+    public GameObject medPrefab;
     public Terrain terrain;
     private TerrainData _terrainData;
-    public Event eggDrop;
 
     private void Start()
     {
         _terrainData = terrain.terrainData;
-        InvokeRepeating("CreateEgg", 1, 20f);
+        InvokeRepeating("CreateEgg", 1, 5f);
+        InvokeRepeating("CreateMedKit", 1, 5f);
     }
 
     private void CreateEgg()
@@ -22,6 +23,15 @@ public class EggSpawner : MonoBehaviour
         Vector3 pos = new Vector3(x, 0 , z);
         pos.y = terrain.SampleHeight(pos) + 10;
         GameObject egg = Instantiate(eggPrefab, pos, Quaternion.identity);
-        eggDrop.Occured(egg);
+        
+    }
+
+    private void CreateMedKit()
+    {
+        int x = (int) Random.Range(0, _terrainData.size.x);
+        int z = (int) Random.Range(0, _terrainData.size.z);
+        Vector3 pos = new Vector3(x, 0 , z);
+        pos.y = terrain.SampleHeight(pos) + 10;
+        GameObject med = Instantiate(medPrefab, pos, Quaternion.identity);  
     }
 }
